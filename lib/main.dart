4fe5335a2f0dbe7cbb9def43ce66d573ac637166
Vinicius Rubens Pedrinho <vrpedrinho@gmail.com>
2023-04-22 11:38:32 -0300

    modified:   lib/main.dart 	modified:   pubspec.lock

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,52 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:provider/provider.dart';
+
+const _brandBlue = Color(0xFF1E88E5);
+//bool _isDemoUsingDynamicColors = false;
+
+CustomColors lightCustomColors = const CustomColors(danger: Color(0xFFE53935));
+CustomColors darkCustomColors = const CustomColors(danger: Color(0xFFEF9A9A));
+
+var brightness =
+    SchedulerBinding.instance.platformDispatcher.platformBrightness;
+bool isLightMode = brightness == Brightness.light;
+
+@immutable
+class CustomColors extends ThemeExtension<CustomColors> {
+  const CustomColors({
+    required this.danger,
+  });
+
+  final Color? danger;
+
+  @override
+  CustomColors copyWith({Color? danger}) {
+    return CustomColors(
+      danger: danger ?? this.danger,
+    );
+  }
+
+  @override
+  CustomColors lerp(ThemeExtension<CustomColors>? other, double t) {
+    if (other is! CustomColors) {
+      return this;
+    }
+    return CustomColors(
+      danger: Color.lerp(danger, other.danger, t),
+    );
+  }
+
+  CustomColors harmonized(ColorScheme dynamic) {
+    return copyWith(danger: danger!.harmonizeWith(dynamic.primary));
+  }
+}
 
 void main() {
   runApp(
@@ -45,7 +88,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   late final WebViewController _controller;
-  String finalUrl = 'https://uol.com';
+  String finalUrl = 'https://flutter.dev';
   final _textController = TextEditingController();
 
   @override
@@ -62,7 +105,6 @@ class _HomeState extends State<Home> {
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setBackgroundColor(Colors.black)
       ..loadRequest(Uri.parse(finalUrl));
-
     AndroidWebViewController.enableDebugging(true);
     (controller.platform as AndroidWebViewController)
         .setMediaPlaybackRequiresUserGesture(false);
@@ -75,6 +117,7 @@ class _HomeState extends State<Home> {
 
   Future<bool> _onBack(BuildContext context) async {
     var canGoBack = await _controller.canGoBack();
+
     if (canGoBack) {
       _controller.goBack();
       return false;
@@ -115,19 +158,60 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return DynamicColorBuilder(
       builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        ColorScheme lightColorScheme;
+        ColorScheme darkColorScheme;
+        late Color addressBarColor;
+        if (lightDynamic != null && darkDynamic != null) {
+          // On Android S+ devices, use the provided dynamic color scheme.
+          // (Recommended) Harmonize the dynamic color scheme' built-in semantic colors.
+          lightColorScheme = lightDynamic.harmonized();
+          // (Optional) Customize the scheme as desired. For example, one might
+          // want to use a brand color to override the dynamic [ColorScheme.secondary].
+          lightColorScheme = lightColorScheme.copyWith(secondary: _brandBlue);
+          // (Optional) If applicable, harmonize custom colors.
+          lightCustomColors = lightCustomColors.harmonized(lightColorScheme);
+
+          // Repeat for the dark color scheme.
+          darkColorScheme = darkDynamic.harmonized();
+          darkColorScheme = darkColorScheme.copyWith(secondary: _brandBlue);
+          darkCustomColors = darkCustomColors.harmonized(darkColorScheme);
+
+          //_isDemoUsingDynamicColors = true; // ignore, only for demo purposes
+        } else {
+          // Otherwise, use fallback schemes.
+          lightColorScheme = ColorScheme.fromSeed(
+            seedColor: _brandBlue,
+          );
+          darkColorScheme = ColorScheme.fromSeed(
+            seedColor: _brandBlue,
+            brightness: Brightness.dark,
+          );
+        }
+
+        if (isLightMode) {
+          debugPrint('Light mode');
+          addressBarColor = lightColorScheme.primaryContainer;
+        } else if (!isLightMode) {
+          debugPrint('Dark mode');
+          addressBarColor = darkColorScheme.primaryContainer;
+        }
         return ChangeNotifierProvider(
           create: (context) => BackEventNotifier(),
           child: MaterialApp(
             themeMode: themeMode,
             theme: ThemeData(
-              colorScheme: lightDynamic ?? Theme.of(context).colorScheme,
-              brightness: Brightness.light,
+              colorScheme: lightDynamic ??
+                  Theme.of(context)
+                      .colorScheme
+                      .copyWith(brightness: Brightness.light),
               useMaterial3: true,
             ),
             darkTheme: ThemeData(
-              colorScheme: darkDynamic ?? Theme.of(context).colorScheme,
+              colorScheme: darkDynamic ??
+                  Theme.of(context)
+                      .colorScheme
+                      .copyWith(brightness: Brightness.dark),
               useMaterial3: true,
-              brightness: Brightness.dark,
             ),
             home: WillPopScope(
               onWillPop: () => _onBack(context),
@@ -154,10 +238,48 @@ class _HomeState extends State<Home> {
                             borderRadius: BorderRadius.circular(80),
                             child: TextField(
                               controller: _textController,
-                              decoration: const InputDecoration(
+                              decoration: InputDecoration(
                                 filled: true,
-                                hintText: 'Enter a URL or search',
-                                border: OutlineInputBorder(
+                                fillColor: isLightMode
+                                    ? lightColorScheme.primaryContainer
+                                    : darkColorScheme.primaryContainer,
+                                contentPadding:
+                                    const EdgeInsets.symmetric(horizontal: 20),
+                                labelText: 'blablabla',
+                                labelStyle: TextStyle(
+                                  background: Paint()
+                                    ..color = addressBarColor
+                                    ..strokeWidth = 30
+                                    ..strokeJoin = StrokeJoin.round
+                                    ..strokeCap = StrokeCap.round
+                                    ..style = PaintingStyle.stroke,
+                                ),
+                                suffixIcon:
+                                    PopupMenuButton(itemBuilder: (context) {
+                                  return [
+                                    const PopupMenuItem<int>(
+                                      value: 0,
+                                      child: Text("parangoricotirimijuaro"),
+                                    ),
+                                    const PopupMenuItem<int>(
+                                      value: 1,
+                                      child: Text("Settings"),
+                                    ),
+                                    const PopupMenuItem<int>(
+                                      value: 2,
+                                      child: Text("Logout"),
+                                    ),
+                                  ];
+                                }, onSelected: (value) {
+                                  if (value == 0) {
+                                    debugPrint("My account menu is selected.");
+                                  } else if (value == 1) {
+                                    debugPrint("Settings menu is selected.");
+                                  } else if (value == 2) {
+                                    debugPrint("Logout menu is selected.");
+                                  }
+                                }),
+                                border: const OutlineInputBorder(
                                   borderRadius: BorderRadius.all(
                                     Radius.circular(80),
                                   ),
@@ -165,7 +287,12 @@ class _HomeState extends State<Home> {
                                 ),
                               ),
                               onSubmitted: (value) {
-                                finalUrl = value;
+                                String finalUrl = value.toLowerCase();
+                                if (!finalUrl.startsWith('http://') &&
+                                    !finalUrl.startsWith('https://')) {
+                                  finalUrl = 'https://$finalUrl';
+                                }
+                                _textController.text = finalUrl;
                                 _controller.loadRequest(Uri.parse(finalUrl));
                               },
                             ),
